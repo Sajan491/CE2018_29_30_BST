253e6b660b34a3b00a1b666eb87ca1e209fdd872
sajan491 <sajanmahat491@gmail.com>
2019-12-26 10:55:09 +0545

    dfsdf

--- bintreelinked.cpp
+++ bintreelinked.cpp
@@ -1,1 +1,45 @@
+#include "bintreelinked.h"
+#include<iostream>
+using namespace std;
 
+LinkedBST::LinkedBST(){
+	root=NULL;
+}
+
+LinkedBST::~LinkedBST()
+
+/*LinkedBST::void preordertraversal()
+{
+
+
+}*/
+
+
+
+void LinkedBST::add(int data){
+   Node *newNode=new Node();
+   newNode->data=data;
+   newNode->left=Null;
+   newNode->right=NULL;
+
+}
+
+void LinkedBST::add(Node *subTree, Node *newNode){
+
+
+}
+
+
+
+/*LinkedBST::bool searchh(int data){
+
+
+}*/
+
+int main(){
+    LinkedBST li;
+    li.add(2);
+    li.add(3);
+
+
+}
