a390009665cacec85c027e0bb17a03edb5b0f56c
sajan491 <sajanmahat491@gmail.com>
2020-02-10 12:00:29 +0545

    Delete complete

--- LinkedBST.cpp
+++ LinkedBST.cpp
@@ -61,40 +61,79 @@ if (root->data==0){
 }
 
 
-void LinkedBST::deleteitem(node *root, int data) {
-	if(root == NULL) return; 
-	else if(data < root->data){
-	    deleteitem(root->left,data);
-	    
-	}
-	else if (data > root->data){
-	    deleteitem(root->right,data);
-	    
-	}
-	else {
-		if(root->left == NULL && root->right == NULL) { 
-			delete root;
-			root = NULL;
-		}
-		else if(root->left == NULL) {
-			node *temp = root;
-			root = root->right;
-			delete temp;
-		}
-		else if(root->right == NULL) {
-			node *temp = root;
-			root = root->left;
-			delete temp;
-		}
-		else { 
-			node *temp;
-			temp->data = minimum(root->right);
-			root->data = temp->data;
-			deleteitem(root->right,temp->data);
-		}
-	}
 
-}
+
+
+void LinkedBST::deleteitem(node* root,int val){
+	
+	int dat;
+	if(root==NULL){
+		cout<<"tree is empty"<<endl;
+		return;
+	}
+	if(search(root,val)==true){
+		if(val<root->data){
+			node* temp=new node();
+			temp=root->left;
+			if(root->left->data==val && root->left->left==NULL && root->left->right==NULL){
+				root->left=NULL;
+			}
+			deleteitem(temp,val);
+			return;
+		}
+		else if(val>root->data){
+			node* temp=new node();
+			temp=root->right;
+			if(root->right->data==val && root->right->left==NULL && root->right->right==NULL){
+				root->right=NULL;
+			}
+			deleteitem(temp,val);
+			return;
+		}
+		else{
+			if(root->left==NULL && root->right==NULL){
+				delete root;
+				root=NULL;
+				return;
+			}
+			else if(root->left==NULL){
+				node* temp=new node();
+				temp=root->right;
+				root->data=root->right->data;
+				root->right=root->right->right;
+				delete temp;
+				return;
+			}
+			else if(root->right==NULL){
+				node* temp=new node();
+				temp=root->left;
+				root->data=root->left->data;
+				root->left=root->left->left;
+				delete temp;
+				return;
+			}
+			else{
+				node* nodetoDelete=new node();
+				nodetoDelete=root;
+				node* newNode=new node();
+				newNode=root->left;
+				while(newNode->right!=NULL){
+					newNode=newNode->right;
+				}
+				dat=newNode->data;
+				deleteitem(nodetoDelete,newNode->data);
+				root->data=dat;
+				return;
+			}
+		}
+	}
+	
+	else{
+		cout<<"The node to be deleted not found in the tree\n";
+		exit(0);
+		}
+}		
+	
 
 bool LinkedBST::search(node *root,int targetKey){
     if(root->data==0){
@@ -111,17 +150,17 @@ bool LinkedBST::search(node *root,int targetKey){
                 p=p->left;
             }
             else if(targetKey==p->data){
-                cout<<targetKey<<" is in the tree"<<endl;
+                
                 return 1;
 
             }
             else{
-                cout<<targetKey<<" is not in the tree"<<endl;
+                
                 return 0;
             }
         }
     }
-    cout<<targetKey<<" is not in the tree"<<endl;
+
     return 0;
 }
 
@@ -210,23 +249,31 @@ int main(){
     cout<<"the max term in the tree is "<<a<<endl;
     
     b = tree1.minimum(&tree1.root);
-    cout<<"the min term in the tree is "<<b<<endl;
+    cout<<"the min term in the tree is "<<b<<endl<<endl<<endl;
+	
 
     tree1.deleteitem(&tree1.root,7);
     tree1.deleteitem(&tree1.root,5);
+	cout<<"After deleting 5 and 7 "<<endl;
     cout<<"Inorder Traversal of the tree is"<<endl;
 	tree1.inordertraversal(&tree1.root);
 	cout<<endl;
-    
+	cout<<"Preorder Traversal of the tree is"<<endl;
+	tree1.preordertraversal(&tree1.root);
+	cout<<endl;
+
 	int number;
 	char character;
+	bool s;
 	search:
 	cout<<"Enter any number you want to search for in the tree"<<endl;
 	cin>>number;
-	tree1.search(&tree1.root,number);
+	s=tree1.search(&tree1.root,number);
+	if(s==1){cout<<number<<" " <<"is in the tree"<<endl;}
+	else{cout<<number<<" " <<"is not in the tree"<<endl;}
 	cout<<"Do you want to search again(y/n)??"<<endl;
 	cin>>character;
 	if((character=='y') || (character=='Y')) goto search;
 
 
-}
+	}
